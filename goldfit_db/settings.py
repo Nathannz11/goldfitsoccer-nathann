2a92d137acbf1ec8f27d25b62ab591589004b37a
Nathannz11 <zininmw@gmail.com>
2023-11-13 22:35:54 -0300

    teste

--- goldfit_db/settings.py
+++ goldfit_db/settings.py
@@ -40,11 +40,11 @@ DEFAULT_REQUEST_HEADERS = {
 
 # Application definition
 
-CSRF_COOKIE_SECURE = False
+CSRF_COOKIE_SECURE = True
 
-CSRF_COOKIE_HTTPONLY = False
 
-CSRF_USE_SESSIONS = False
+
+CSRF_USE_SESSIONS = True
 
 INSTALLED_APPS = [
     'django.contrib.admin',
